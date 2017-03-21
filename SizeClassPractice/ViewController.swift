f1c39c9f08dbefdaaa1984df5e106380a5ef11c0
sofiaknezevic <sofia.knezevic@gmail.com>
2017-03-21 11:44:26 -0700

    Final commit, short project

---
+++ SizeClassPractice/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  SizeClassPractice
+//
+//  Created by Sofia Knezevic on 2017-03-21.
+//  Copyright © 2017 Sofia Knezevic. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+
+
+}
+
